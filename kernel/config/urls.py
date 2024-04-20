5b69d1103d01d6c0db4473b0e1e5ec08bdfd4ed2
Adrian Makridenko <adrian.makridenko@proton.me>
2024-04-20 16:13:27 +0300

    [*] Обновил структуру

--- kernel/config/urls.py
+++ kernel/config/urls.py
@@ -4,15 +4,16 @@ from django.contrib import admin
 from django.urls import path, re_path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.decorators.csrf import csrf_exempt, ensure_csrf_cookie
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
 urlpatterns += [
-    re_path(r'', 
-        ensure_csrf_cookie(TemplateView.as_view(template_name='index.html')), 
+    re_path(r'',
+        ensure_csrf_cookie(TemplateView.as_view(template_name='index.html')),
         name='index',
     )
 ]

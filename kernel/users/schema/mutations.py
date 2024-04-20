75ef1f432aef84c47c8ca05bebf493cb797fed08
Adrian Makridenko <adrian.makridenko@proton.me>
2024-04-21 02:44:32 +0300

    [+] Мутация на регистрацию пользователя

--- kernel/users/schema/mutations.py
+++ kernel/users/schema/mutations.py
@@ -8,7 +8,10 @@ from graphene import relay, ObjectType
 from graphene_django.types import DjangoObjectType
 from users.models import KeyLogin, Profile
 
-from users.services import create_keylogin
+from users.services import (create_keylogin,
+                            register_new_profile as reg,
+                            gen_seed as seed,
+                            gen_jwt as gjwt)
 
 
 class KeyLogin(DjangoObjectType):
@@ -24,7 +27,35 @@ class ProfileNode(DjangoObjectType):
         interfaces = (relay.Node,)
 
     user_id = graphene.Int()
-    username = graphene.String()
+    email = graphene.String()
+
+
+class RegisterUserMutation(graphene.Mutation):
+
+    profile = graphene.Field(ProfileNode)
+    token = graphene.String()
+
+    class Input:
+        email = graphene.String(required=True)
+        seed = graphene.String(required=True)
+
+    @staticmethod
+    def mutate(
+        root: Any,
+        info: graphene.ResolveInfo,
+        **input: Dict[str, Any]
+    ):
+
+        try:
+            profile = reg(email=input['email'],
+                          seed=input['seed'])
+        except ValidationError as errors:
+            error_list = [str(error) for error in errors]
+            raise GraphQLError(message='\n'.join(error_list))
+
+        token = gjwt(profile=profile)
+
+        return RegisterUserMutation(profile=profile, token=token)
 
 class KeyLoginMutation(graphene.Mutation):
     """
@@ -62,3 +93,4 @@ class KeyLoginMutation(graphene.Mutation):
 
 class Mutations(ObjectType):
     keylogin = KeyLoginMutation.Field()
+    register_user = RegisterUserMutation.Field()
--- kernel/users/services/__init__.py
+++ kernel/users/services/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 
 from .keyslogin import create_keylogin
+from .profile_services import gen_jwt, gen_seed, register_new_profile

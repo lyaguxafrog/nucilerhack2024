2525d9d8fd2615f2f35851df15c1b0bc86a486e4
Adrian Makridenko <adrian.makridenko@proton.me>
2024-04-21 02:18:29 +0300

    [+] Добавил JWT-запросы

--- kernel/config/schema.py
+++ kernel/config/schema.py
@@ -2,6 +2,8 @@
 
 import graphene
 from graphene import ObjectType, Schema
+import graphql_jwt
+
 
 from users.schema import Mutation as UserMutation
 
@@ -16,8 +18,9 @@ class Query(ObjectType):
 class Mutation(
     UserMutation,
     ObjectType,
-): pass
-
+):
+    verify_token = graphql_jwt.Verify.Field()
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
 
 
 schema = Schema(mutation=Mutation, query=Query)

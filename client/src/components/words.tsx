d8825c488e8b68c737d9cc990410d7aa2ebdd4d5
Kikuy0 <al6ity@gmail.com>
2024-04-21 11:12:56 +0300

    added words enter page

---
+++ client/src/components/words.tsx
@@ -0,0 +1,24 @@
+
+export const Words = () => {
+    return (
+      <section className="register">
+        <h2 className="registerTitle">WebAuthn</h2>
+        <div className="wordsRow">
+        <input type="text" className="input"/>/
+        <input type="text" className="input"/>/
+        <input type="text" className="input"/>/
+        <input type="text" className="input"/>/
+        <input type="text" className="input"/>/
+        <input type="text" className="input"/>/
+        <input type="text" className="input"/>/
+        <input type="text" className="input"/>/
+        <input type="text" className="input"/>/
+        <input type="text" className="input"/>/
+        <input type="text" className="input"/>/
+        <input type="text" className="input"/>
+        </div>
+        <button className="button"></button>
+      </section>
+    );
+  };
+  

5ccfe498e43558f24d4d3407ac019e346aff13d1
Adrian Makridenko <adrian.makridenko@proton.me>
2024-04-21 04:24:10 +0300

    [+] Приложение cloud
    R: Нужна для синхронизации

---
+++ kernel/cloud/apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from django.apps import AppConfig
+
+
+class CloudConfig(AppConfig):
+    name = 'cloud'
+    label = 'cloud'
--- kernel/config/local_settings.py
+++ kernel/config/local_settings.py
@@ -4,4 +4,10 @@ ALLOWED_HOSTS = ['*']
 
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
+]# -*- coding: utf-8 -*-
+
+ALLOWED_HOSTS = ['*']
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
 ]

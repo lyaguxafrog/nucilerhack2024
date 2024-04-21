1b287732cef27d319101a3b3b9e66f2f633c9c9e
Adrian Makridenko <adrian.makridenko@proton.me>
2024-04-21 05:47:36 +0300

    [-] Удалил мутации и модель KeyLogin
    R: Они не нужны в users и их нужно изменить

--- kernel/users/schema/mutations.py
+++ kernel/users/schema/mutations.py
@@ -8,17 +8,10 @@ from graphene import relay, ObjectType
 from graphene_django.types import DjangoObjectType
 from users.models import KeyLogin, Profile
 
-from users.services import (create_keylogin,
-                            register_new_profile as reg,
+from users.services import (register_new_profile as reg,
                             gen_seed as seed,
                             gen_jwt as gjwt)
 
-
-class KeyLogin(DjangoObjectType):
-    class Meta:
-        model = KeyLogin
-    key = graphene.String()
-    login = graphene.String()
 
 
 class ProfileNode(DjangoObjectType):
@@ -57,40 +50,7 @@ class RegisterUserMutation(graphene.Mutation):
 
         return RegisterUserMutation(profile=profile, token=token)
 
-class KeyLoginMutation(graphene.Mutation):
-    """
-    Мутация для добавления ключа для сервиса.
-
-    Пример запроса:
-
-    mutation {
-      sendKeyLogin(key: "Публичный ключ", login: "URL сервиса") {
-          answer
-      }
-    }
-
-    """
-    class Input:
-        key = graphene.String(required=True)
-        login = graphene.String(required=True)
-
-    # class Arguments:
-    #     keylogin_data = Input(required=True)
-
-    keylogin = graphene.Field(KeyLogin)
-
-    def mutate(root: Any,
-        info: graphene.ResolveInfo,
-        **input: Dict[str, Any]):
-
-        keylogin = create_keylogin(
-            key=input['key'],
-            login=input['login']
-        )
-        ok = True
-        return KeyLoginMutation(keylogin=keylogin)
 
 
 class Mutations(ObjectType):
-    keylogin = KeyLoginMutation.Field()
     register_user = RegisterUserMutation.Field()
--- kernel/users/services/__init__.py
+++ kernel/users/services/__init__.py
@@ -1,4 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from .keyslogin import create_keylogin
 from .profile_services import gen_jwt, gen_seed, register_new_profile

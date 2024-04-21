d4a15ed35ce6397c000f16631d6066a6c186df73
Adrian Makridenko <adrian.makridenko@proton.me>
2024-04-21 12:46:46 +0300

    [+] Мутация для создания сигнатуры

--- kernel/authn/schema/schema.py
+++ kernel/authn/schema/schema.py
@@ -2,8 +2,15 @@
 
 from graphene import ObjectType
 
+from .mutations import Mutation as SignMutations
 
 class Query(
     ObjectType,
+):
+    pass
+
+class Mutations(
+    SignMutations,
+    ObjectType
 ):
     pass

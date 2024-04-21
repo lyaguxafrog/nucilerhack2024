a41feacd0a5a2513f939181a6fc68d50db63ed32
talkingmachine <101518410+talkingmachine@users.noreply.github.com>
2024-04-21 05:44:05 +0300

    [-] remove client2

--- client2/src/components/not-found.tsx
+++
@@ -1,8 +0,0 @@
-
-export const NotFoundPage = () => {
-  return (
-      <section className="notFound">
-        
-      </section>
-    );
-  };
--- client2/src/consts/router-paths.ts
+++
@@ -1,5 +0,0 @@
-export const RouterPaths = {
-  root: ():string => '/',
-  product: (id: number | string):string => `/product/${id}`,
-  notFound: ():string => '*',
-};
